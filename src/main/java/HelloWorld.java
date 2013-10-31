daa39eef672f41f075fbe8222c33f89a91813684
G_Star <308904931@qq.com>
2013-10-31 15:19:54 +0800

    init

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: G_Star
+ * Date: 13-10-31
+ * Time: 下午3:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+
+        System.out.println("Hello World");
+    }
+}
